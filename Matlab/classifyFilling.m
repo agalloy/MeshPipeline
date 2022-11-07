a58b7bc46aaaa904f84c3ad40a29a6de876f9796
agalloy <78229249+agalloy@users.noreply.github.com>
2022-11-07 12:42:33 -0600

    Update classifyFilling.m
    Improved the classify filling function to work with more general data.

--- Matlab/classifyFilling.m
+++ Matlab/classifyFilling.m
@@ -4,129 +4,82 @@
 %% Initialize MATLAB
 clear
 clc
-addpath(GIBBONPATH())
 
 %% Initial parameters
-leftLungNames = ["LUL", "LLL"];
-rightLungNames = ["RUL", "RML", "RLL"];
-leftLungID = [1, 2, 6];
-rightLungID = [3, 4, 5, 7];
+lobeID = [1,2,3,4,5];
+fillID = 6;
 patientID = input('Input patient identification number: ', 's');
 
 %% Step 1: Loading a lung segmentation 
 
 % Selecting a file
-default_dir = 'My/Directory/With/Images';
-[image_name, image_dir] = uigetfile('.hdr', 'Select an Image File', default_dir);
-image_path = fullfile(image_dir, image_name);
+default_dir = pwd();
+[mask_name, mask_dir] = uigetfile('', 'Select an Image File', default_dir);
+mask_path = fullfile(mask_dir, mask_name);
 
 % Get image extension
-[~,~,image_ext] = fileparts(image_path);
+[~,~,mask_ext] = fileparts(mask_path);
 
 % Use the appropriate commands to open the file depending on the extension
-if strcmp('.hdr',image_ext)
+if strcmp('.hdr',mask_ext)
     % Get the meta data for the analyze 75 formatted image
-    image_info = analyze75info(image_path);
+    mask_info = analyze75info(mask_path);
 
     % Read the analyze 75 formatted image
-    image = analyze75read(image_info);
+    mask = analyze75read(mask_info);
     
-elseif strcmp('.nii',image_ext)
+elseif strcmp('.nii',mask_ext)
     % Read nifti file
-    image = niftiread(image_path);
+    mask = niftiread(mask_path);
     
     % Get nifti meta-data
-    image_info = niftiinfo(image_path);
+    mask_info = niftiinfo(mask_path);
+    
+else
+    % Read nifti file
+    mask = niftiread(mask_path);
+    
+    % Get nifti meta-data
+    mask_info = niftiinfo(mask_path);
         
 end
 
 % Get voxel size
-voxel_size = image_info.PixelDimensions(1:3);
+voxel_size = mask_info.PixelDimensions(1:3);
 
 %% Step 2: Classifying cavity filling
+% Get the filling voxels
+fill_voxels = find ( ismember(mask,fillID) );
 
-% Left lung 
-
-lung_image = ismember(image, leftLungID);
-
-LUL_mask = image == leftLungID(1); 
-LLL_mask = image == leftLungID(2); 
-FL_mask = image == leftLungID(3); 
-
-LUL_dist = bwdist(LUL_mask);
-LLL_dist = bwdist(LLL_mask);
-
-FL_voxels = find(FL_mask);
-
-for i = 1:length(FL_voxels)
-    upperDist = LUL_dist(FL_voxels(i));
-    lowerDist = LLL_dist(FL_voxels(i));
-    if upperDist < lowerDist
-        image(FL_voxels(i)) = leftLungID(1); 
-    else 
-        image(FL_voxels(i)) = leftLungID(2); 
-    end 
-end 
-
-% Right lung 
-
-lung_image = ismember(image, rightLungID);
-
-RUL_mask = image == rightLungID(1); 
-RML_mask = image == rightLungID(2); 
-RLL_mask = image == rightLungID(3);
-FR_mask = image == rightLungID(4); 
-
-RUL_dist = bwdist(RUL_mask);
-RML_dist = bwdist(RML_mask);
-RLL_dist = bwdist(RLL_mask);
-
-FR_voxels = find(FR_mask);
-
-for i = 1:length(FR_voxels)
-    upperDist = RUL_dist(FR_voxels(i));
-    middleDist = RML_dist(FR_voxels(i));
-    lowerDist = RLL_dist(FR_voxels(i));   
+% Loop through every lobe and find distance from filling to that lobe
+% Initialize arrays
+fill_dist = ones(size(fill_voxels)) * 10^6;
+fill_lobe = nan(size(fill_voxels));
+for i = 1:size(lobeID,2)
+    % Get the mask of the current lobe
+    lobe_mask = mask == lobeID(i);
+    % Compute the distance function wrt the current lobe
+    lobe_dist = bwdist(lobe_mask);
+    % Evaluate the distance at each filled voxel from the current lobe
+    fill_dist_new = lobe_dist(fill_voxels);
     
-    if (upperDist < middleDist) && (upperDist < lowerDist)
-        image(FR_voxels(i)) = rightLungID(1); 
-    elseif (middleDist < lowerDist) 
-        image(FR_voxels(i)) = rightLungID(2); 
-    else 
-        image(FR_voxels(i)) = rightLungID(3);
-    end 
-end 
+    % Find the voxels closer to the current lobe than any previous one
+    closer = fill_dist > fill_dist_new;
+    % Update the closest lobe and distance for those voxels 
+    fill_dist(closer) = fill_dist_new(closer);
+    fill_lobe(closer) = lobeID(i);
+end
 
-%Get image size
-image_size = size(image);
+% Classify the filled voxel regions according to their closest lobe
+mask(fill_voxels) = fill_lobe;
 
-%Extract an image slice
-%Note that by convention the "X" coordinate typically referes to the 2nd image index
-%and the "Y" coordinate typically refers to the 1st
-sliceXZ = image(150,:,:);
-sliceYZ = image(:,150,:);
-sliceXY = image(:,:,150);
-
-%The squeeze function removes array dimensions with a length of 1 to make
-%our images truly 2D
-sliceXZ = squeeze(sliceXZ);
-sliceYZ = squeeze(sliceYZ);
-sliceXY = squeeze(sliceXY);
-
-%Start a new figure displaying the three slices
+%% View filled segmentation slices
 figure()
-subplot(1,3,1)
-imshow(sliceXZ',[])
-xlabel('XZ (Coronal) Slice')
-subplot(1,3,2)
-imshow(sliceYZ',[])
-xlabel('YZ (Saggital) Slice')
-subplot(1,3,3)
-imshow(sliceXY,[])
-xlabel('XY (Axial) Slice')
+orthosliceViewer(mask)
+
 
 %% Export to .nii file
 
 filename = [patientID, '_baseTLC_lobemask_filled.nii'];
-niftiwrite(image, filename, image_info);
+niftiwrite(mask, filename, mask_info);
 
