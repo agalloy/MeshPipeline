18d86c8cdc319b74e6f8556e36675ca4ef24a3c4
agalloy <78229249+agalloy@users.noreply.github.com>
2023-01-26 17:01:37 -0600

    Added support for new displacement field data.
    Modified SampleDispField.m to support displacement field data from SPIROMICS.

--- Matlab/SampleDispField.m
+++ Matlab/SampleDispField.m
@@ -8,24 +8,43 @@
 % Outputs:
 %    disp = An [N X 3] array of displacements located at each desired point
 
-function disp = SampleDispField(X,disp_pattern)
+function dispArray = SampleDispField(X,disp_pattern)
     % Initialize displacement array
-    disp = nan( size(X,1), 3 );
+    dispArray = nan( size(X,1), 3 );
     
-    % Coordinate axes to loop through
-    axes = {'x','y','z'};
-    axes_ind = [2,1,3];
+    % Check if disp field components are together in one file or seperate
+    if ~contains(disp_pattern,'${AXIS}')
+        % Open full disp file
+        dvf_info = niftiinfo(disp_pattern);
+        dvf_full = niftiread(disp_pattern);
+        dvf_spacing = dvf_info.PixelDimensions(1:3);
+        together = true;        
+        % Coordinate axes to loop through
+        axes = {'x','y','z'};
+        axes_ind = [2,1,3];
+    else
+        % Coordinate axes to loop through
+        axes = {'x','y','z'};
+        axes_ind = [2,1,3];
+        together = false;
+    end
     
     % Loop through each axis sampling disp data
     for i = 1:numel(axes)
-        % Get the displacement vector field (dvf) for the given axis
-        dvf_path = replace(disp_pattern,'${AXIS}',axes{i});
-        % Read metadata
-        dvf_info = analyze75info(dvf_path);
-        dvf_spacing = dvf_info.PixelDimensions(1:3);
-        % Load dvf
-        dvf = analyze75read(dvf_info);
-        dvf_size = size(dvf);
+        if ~together
+            % Get the displacement vector field (dvf) for the given axis
+            dvf_path = replace(disp_pattern,'${AXIS}',axes{i});
+            % Read metadata
+            dvf_info = analyze75info(dvf_path);
+            dvf_spacing = dvf_info.PixelDimensions(1:3);
+            % Load dvf for the current axis
+            dvf = analyze75read(dvf_info);
+            dvf_size = size(dvf);
+        else
+            % Get only the dvf for the current axis
+            dvf = dvf_full(:,:,:,1,i);
+            dvf_size = size(dvf);
+        end
         
         % Convert the voxel centers to a set of points
         ind_i = 1:dvf_size(1);
@@ -35,8 +54,8 @@ function disp = SampleDispField(X,disp_pattern)
         Py = double( (ind_i-1) * dvf_spacing(1) );
         Pz = double( (ind_k-1) * dvf_spacing(3) );
         Pu = dvf;
-                
+
         % Get displacements at query points with linear interpolation
-        disp(:,axes_ind(i)) = interp3(Py,Px,Pz,Pu,X(:,2),X(:,1),X(:,3),'linear');
+        dispArray(:,axes_ind(i)) = interp3(Px,Py,Pz,Pu,X(:,1),X(:,2),X(:,3),'linear');
     end
 end
